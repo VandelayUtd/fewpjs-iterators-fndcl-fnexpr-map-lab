087d641a2c82dbd7348f9d95596890e8235c356b
VandelayUtd <drewhadley.guitar@gmail.com>
2021-10-28 19:15:33 -0400

    Done.

--- index.js
+++ index.js
@@ -13,5 +13,11 @@ const tutorials = [
 
 const titleCased = () => {
   // use the .map method on the tutorials to return a new array
-  return tutorials
+  return tutorials.map( string => {
+    let words = string.split(" ")
+    let capWords = words.map(word => 
+      word.charAt(0).toUpperCase() + word.slice(1)
+    )
+    return capWords.join(" ")
+  })
 }
